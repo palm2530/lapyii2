e97320ad08d52e27187b1b867c83f1700381f50a
palm2530 <palm2530@gmail.com>
2024-03-02 15:45:51 +0700

    test

--- tests/unit/models/UserTest.php
+++ tests/unit/models/UserTest.php
@@ -8,13 +8,20 @@ class UserTest extends \Codeception\Test\Unit {
 
     public function testFindUserById() {
 
-        $this->assertTrue(true);
-        $this->assertFalse(false);
+        // $this->assertTrue(true);
+        // $this->assertFalse(false);
+
+        verify($user = User::findIdentity(1))->notEmpty();
+        verify($user->username)->equals('admin');
+        verify(User::findIdentity(999))->empty();
+
+        
+
     }
 
-    public function testFindUserByName() {
+    // public function testFindUserByName() {
 
-        $this->assertTrue(true);
-        $this->assertFalse(false);
-    }
+    //     $this->assertTrue(true);
+    //     $this->assertFalse(false);
+    // }
 }
